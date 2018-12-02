7f29b0a5f30db4daff0a6b62dc8d19305baf3247
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-12-02 14:29:42 +0100

    [FCT] init of some (I hope) pertinent settings

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 /***************************************************/
 /***  THE CLASS             ************************/
@@ -20,9 +21,21 @@ public class SettingsManager :
     [System.Serializable]
     public class Settings
     {
-        public int m_testInt;
-        public float m_testFloat;
-        public string m_testString;
+        public int[] categoriePrices;
+
+        /* percent of categoriePrices */
+
+        public double buyerPrice;
+        public double buyerPriceMaxDerivation;
+
+        // buyerIntriguedPrice = actual buyerPrice + buyerIntriguedPriceMaxDerivation
+        public double buyerIntriguedPriceMaxDerivation;
+
+        // buyerHighestPrice = (intrigued ? actual intrigued buyer price : actual buyer price) + buyerHighestPriceMaxDerivation
+        public double buyerHighestPriceMaxDerivation;
+
+        public double grandpaPrice;
+        public double grandpaPriceMaxDerivation;
     }
 
     #endregion
@@ -72,7 +85,11 @@ public class SettingsManager :
     {
         Debug.Assert(m_settingsFile != null, "Settings file not found...");
 
-        m_settings = JsonUtility.FromJson<Settings>(m_settingsFile.text);
+        string clearedString = Regex.Replace(m_settingsFile.text, "//.*\\n", "\n");
+        clearedString = Regex.Replace(clearedString, "/\\**/", "");
+        clearedString = Regex.Replace(clearedString, "/\\*+[^*]*\\*+(?:[^/*][^*]*\\*+)*/", "");
+        
+        m_settings = JsonUtility.FromJson<Settings>(clearedString);
     }
 
     // Update is called once per frame
