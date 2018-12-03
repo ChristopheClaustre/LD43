1e2c5289446ff497830d6d244a5f14e3327c9632
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-12-03 15:31:48 +0100

    [FCT] discussion's script

---
+++ Assets/Scripts/Discussion/Discussions.cs
@@ -0,0 +1,153 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Discussions :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    public enum Who
+    {
+        eGrandpa,
+        eYou
+    }
+
+    public struct Discussion
+    {
+        public Who who;
+        public string text;
+
+        public Discussion(Who p_who, string p_text)
+        {
+            who = p_who;
+            text = p_text;
+        }
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    public static Discussions Inst
+    {
+        get
+        {
+            return m_instance;
+        }
+    }
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    private static Discussions m_instance;
+    public TextMeshProUGUI textMesh_grandpa;
+    public TextMeshProUGUI textMesh_you;
+
+    public Animator m_animator;
+    public string m_actualDiscussionName;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Assert(m_instance == null);
+        m_instance = this;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void ChangeText(Discussion p_discussion)
+    {
+        if (p_discussion.who == Who.eGrandpa)
+            textMesh_grandpa.text = p_discussion.text;
+        else
+            textMesh_you.text = p_discussion.text;
+    }
+
+    public void ClearDiscussion()
+    {
+        ChangeText(new Discussion(Who.eGrandpa, ""));
+        ChangeText(new Discussion(Who.eYou, ""));
+    }
+
+    public void NextDiscussion()
+    {
+        if (m_actualDiscussionName == "") return;
+        m_animator.SetBool("Next"+ m_actualDiscussionName, true);
+    }
+
+    public void ShowNextText()
+    {
+        if (m_actualDiscussionName == "") return;
+        gameObject.SendMessage("Next"+m_actualDiscussionName);
+    }
+
+    public void ResetNextDiscussion()
+    {
+        if (m_actualDiscussionName == "") return;
+        m_animator.SetBool("Next"+m_actualDiscussionName, false);
+    }
+
+    public void BeginDiscussion(string p_name)
+    {
+        m_actualDiscussionName = p_name;
+        m_animator.SetBool("Launch" + m_actualDiscussionName, true);
+    }
+
+    public void EndDiscussion()
+    {
+        m_animator.SetBool("Launch" + m_actualDiscussionName, false);
+        m_actualDiscussionName = "";
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
---
+++ Assets/Scripts/Discussion/Introduction.cs
@@ -0,0 +1,98 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Introduction :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    Discussions.Discussion[] m_discussion =
+    {
+        new Discussions.Discussion( Discussions.Who.eYou, "Hello Grandpa how are you ?" ),
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "Fine Dear... fine..." ),
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "Your father told me that you are here to help me face my debt but..." ),
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "But please don't dilapidate all my memories for cheap" ),
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "My heart will not endure it well..." ),
+        new Discussions.Discussion( Discussions.Who.eYou, "Eviction day is in only 5 days grandpa" ),
+        new Discussions.Discussion( Discussions.Who.eYou, "Sacrifices must be made to save your shop" ),
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "Please... take care of you grandpa... I love you dear" ),
+    };
+
+    int m_round = 0;
+
+    public Animator m_animator;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void NextIntroduction()
+    {
+        Discussions.Inst.ChangeText(m_discussion[m_round]);
+        m_round = (m_round + 1) % m_discussion.Length;
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
