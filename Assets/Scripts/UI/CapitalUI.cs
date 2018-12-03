f90d2e84b29ca18b8f2e4c09e55c74dd04a2aa29
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-12-03 13:16:40 +0100

    [FCT] selling animation (katchak sound playing + update of capital)

---
+++ Assets/Scripts/UI/CapitalUI.cs
@@ -0,0 +1,96 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class CapitalUI :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    public TextMeshProUGUI m_capitalUIText;
+    public AudioSource m_katchakSource;
+    public Animator m_animator;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void UpdateUI()
+    {
+        m_capitalUIText.text = Dora.Inst.GetCapital() + " $";
+    }
+
+    public void PlayKatchak()
+    {
+        m_katchakSource.Play();
+    }
+
+    public void ResetAnimatorBoolean()
+    {
+        m_animator.SetBool("vente", false);
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
