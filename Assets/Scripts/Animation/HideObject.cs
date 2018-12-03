ac0b5ad4ab96ed92f456a675abbaa601b031d0cc
Xavier Chalut <xavier.chalut@gmail.com>
2018-12-03 15:32:34 +0100

    [FCT] Add stuff on object

---
+++ Assets/Scripts/Animation/HideObject.cs
@@ -0,0 +1,73 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class HideObject : MonoBehaviour
+{
+
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseEnter()
+    {
+        this.transform.localScale = this.transform.localScale * 1.1f;
+    }
+
+    void OnMouseExit()
+    {
+        this.transform.localScale = this.transform.localScale / 1.1f;
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void Hide()
+    {
+        this.GetComponent<PolygonCollider2D>().enabled = false;
+        this.GetComponent<Animator>().SetBool("Hide", true);
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+    #endregion
+}
---
+++ Assets/Scripts/Picking2D.cs
@@ -0,0 +1,72 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Picking2D : MonoBehaviour
+{
+
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+            if (hit.collider != null)
+            {
+                GameObject hitGO = hit.collider.gameObject;
+                if (hitGO.tag == "Object")
+                {
+                    Debug.Log(hitGO.name);
+                    hitGO.GetComponent<HideObject>().Hide();
+                }
+
+            }
+        }
+    }
+
+        /********  OUR MESSAGES     ************************/
+
+        /********  PUBLIC           ************************/
+
+        /********  PROTECTED        ************************/
+
+        /********  PRIVATE          ************************/
+        #endregion
+}
