417e20cc55dacc4adf91fb7737c93d90f015ebd1
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-12-05 03:10:44 +0100

    [FCT] add of some more conversation

---
+++ Assets/Scripts/Discussion/Bye.cs
@@ -0,0 +1,100 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Bye :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    Discussions.Discussion[] m_discussion =
+    {
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "We can't find an arrangement. Bye." ),
+        new Discussions.Discussion( Discussions.Who.eYou, "Have a nice day." ),
+    };
+
+    int m_round = 0;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void NextBye()
+    {
+        if (m_round >= m_discussion.Length)
+            Discussions.Inst.EndDiscussion();
+        else
+        {
+            Discussions.Inst.ChangeText(m_discussion[m_round]);
+            m_round++;
+        }
+    }
+
+    public void BeginBye()
+    {
+        m_round = 0;
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
---
+++ Assets/Scripts/Discussion/TooExpensive.cs
@@ -0,0 +1,100 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class TooExpensive :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    Discussions.Discussion[] m_discussion =
+    {
+        new Discussions.Discussion( Discussions.Who.eGrandpa, "It's too expensive" ),
+        new Discussions.Discussion( Discussions.Who.eYou, "I'll see what I can do" ),
+    };
+
+    int m_round = 0;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public void NextTooExpensive()
+    {
+        if (m_round >= m_discussion.Length)
+            Discussions.Inst.EndDiscussion();
+        else
+        {
+            Discussions.Inst.ChangeText(m_discussion[m_round]);
+            m_round++;
+        }
+    }
+
+    public void BeginTooExpensive()
+    {
+        m_round = 0;
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
